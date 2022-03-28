c4d68de8994c1857c97f370054738615cbbb50fc
Deblyn Prado <deblynprado@hotmail.com>
2022-03-28 11:25:11 -0300

    Adding Login Form

---
+++ login-form.php
@@ -0,0 +1,16 @@
+<form action="#" id="login-form" class="login-form">
+  <label for="username" class="sc-only">Username</label>
+  <input type="text" id="username" placeholder="Username">
+  <label for="password" class="sc-only">Password</label>
+  <input type="password" id="password" placeholder="Password">
+</form>
+
+<style>
+  .sc-only {
+    font-size: 1px;
+    color: transparent;
+    position: absolute;
+    top: -9000px;
+    left: -900px;
+  }
+</style>
